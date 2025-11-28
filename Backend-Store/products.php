d7dddbb99f487441ef1111fbbc1f639e11270c82
sharybaydoun <Sharibaydoun60@gmail.com>
2025-11-28 11:15:03 +0200

    Implement endpoint to return all products

---
+++ Backend-Store/products.php
@@ -0,0 +1,13 @@
+<?php
+header("Content-Type: application/json");
+require "db.php";
+
+$result = $conn->query("SELECT * FROM products");
+$products = [];
+
+while ($row = $result->fetch_assoc()) {
+    $products[] = $row;
+}
+
+echo json_encode($products);
+?>
